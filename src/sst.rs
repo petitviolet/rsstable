8fc40770b82cf6c77e4f0dc4f66412aed7d06ca2
petitviolet <violethero0820@gmail.com>
2020-08-06 23:51:49 +0900

    SSTable implementation

---
+++ src/sst.rs
@@ -0,0 +1,39 @@
+//! SSTable(Sorted String Table) in Rust
+//! Basically, this is a Key-Value store on top of local file storage.
+
+use std::{collections::HashMap, fs, path::Path, ops::Deref};
+use crate::sst::memtable::Memtable;
+mod disktable;
+mod memtable;
+
+pub struct SSTable {
+    // memtable: Box<dyn memtable::Memtable<Key = String, Value = String>>,
+    memtable: Box<memtable::default::HashMemtable<String, String>>,
+    disktable: Box<dyn disktable::Disktable>,
+}
+
+impl SSTable {
+    pub fn new(dir_name: impl Into<String>) -> SSTable {
+        SSTable {
+            memtable: Box::new(memtable::default::HashMemtable::new()),
+            disktable: Box::new(
+                disktable::default::FileDisktable::new(dir_name.into()).unwrap(),
+            ),
+        }
+    }
+    pub fn get(&self, key: impl Into<String>) -> Option<String> {
+        let key = key.into();
+        self.memtable.get(&key).map(|res| res.to_string()).or_else(|| self.disktable.find(&key))
+    }
+    pub fn insert(&mut self, key: impl Into<String>, value: impl Into<String>) -> () {
+      let key = key.into();
+      let value = value.into();
+      match self.memtable.set(key, value) {
+        Some((memtable, tombstones)) => { 
+          self.disktable.flush(memtable.deref(), tombstones.deref());
+        },
+        None => {},
+      }
+    }
+
+}
