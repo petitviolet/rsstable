7608da00fbdbbf6612f9dc1bbf9bb2f334218de2
petitviolet <violethero0820@gmail.com>
2020-08-09 23:18:47 +0900

    added main

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod sst;
+use sst::SSTable;
+
+fn main() {
+    let mut sst = SSTable::new("./tmp");
+    sst.clear().expect("failed to clear");
+    (1..10).for_each(|i| {
+        println!("i: {} =====", i);
+        let key = || format!("key-{}", i);
+        let value = || format!("value-{}", i);
+        println!("get: {:?}", sst.get(key()));
+        println!("insert: {:?}", sst.insert(key(), value()));
+        println!("get: {:?}", sst.get(key()));
+    });
+    ()
+}
