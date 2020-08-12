7950eabc55ce83116f8794b56c0edf80b982bab6
petitviolet <violethero0820@gmail.com>
2020-08-12 23:33:50 +0900

    minor updated main sample

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use sst::SSTable;
 
 fn main() {
     let mut sst = SSTable::new("./tmp", 3);
-    sst.clear().expect("failed to clear");
+    // sst.clear().expect("failed to clear");
     (1..10).for_each(|i| {
         println!("i: {} =====", i);
         let key = || format!("key-{}", i);
         let value = || format!("value-{}", i);
-        println!("get: {:?}", sst.get(key()));
+        println!("get({}): {:?}", i, sst.get(key()));
         println!("insert: {:?}", sst.insert(key(), value()));
-        println!("get: {:?}", sst.get(key()));
+        println!("get({}): {:?}", i, sst.get(key()));
+        println!("get({}): {:?}", i + 4, sst.get(format!("key-{}", i + 4)));
     });
     ()
 }
