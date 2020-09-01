8280b425f2822c4d7bb47901335b644ae98435e8
petitviolet <violethero0820@gmail.com>
2020-09-01 23:45:35 +0900

    enriched test case

--- src/sst.rs
+++ src/sst.rs
@@ -64,43 +64,43 @@ mod tests {
         let key = |i| format!("key-{}", i);
         let value = |i| format!("value-{}", i);
 
-        {
-            let mut sst = SSTable::new("./test_tmp", 3);
-            assert!(sst.clear().is_ok());
-            // get -> set -> get
-            (1..=10).for_each(|i| {
-                println!("{} ------", i);
-                assert_eq!(sst.get(key(i)), None);
-                sst.insert(key(i), value(i)).expect("success");
-                assert_eq!(sst.get(key(i)), Some(value(i)));
-            });
-            // get -> delete -> get
-            (1..=10).for_each(|i| {
-                assert_eq!(sst.get(key(i)), Some(value(i)));
-                sst.delete(key(i));
-                assert_eq!(sst.get(key(i)), None);
-            });
-            // get
-            (1..=10).for_each(|i| {
-                assert_eq!(sst.get(key(i)), None);
-            });
-        }
+        let mut sst = SSTable::new("./test_tmp", 200);
+        assert!(sst.clear().is_ok());
+        // get -> set -> get
+        (1..300).for_each(|i| {
+            assert_eq!(sst.get(key(i)), None);
+            sst.insert(key(i), value(i)).expect("success");
+            assert_eq!(sst.get(key(i)), Some(value(i)));
+        });
+        // get -> delete -> get
+        (1..300).for_each(|i| {
+            assert_eq!(sst.get(key(i)), Some(value(i)));
+            sst.delete(key(i));
+            assert_eq!(sst.get(key(i)), None);
+        });
+        // get
+        (1..300).for_each(|i| {
+            assert_eq!(sst.get(key(i)), None);
+        });
+    }
 
-        {
-            let mut sst = SSTable::new("./test_tmp", 3);
-            assert!(sst.clear().is_ok());
-            (1..=5).for_each(|i| {
-                sst.insert(key(i), value(i)).expect("success");
-            });
-            sst.delete(key(2));
-            // restore WAL
-            // memtable: [4, 5], tombstone: [2], disktable: [1, 2, 3]
-            let mut sst = SSTable::new("./test_tmp", 3);
-            assert_eq!(sst.get(key(1)), Some(value(1)));
-            assert_eq!(sst.get(key(2)), None);
-            assert_eq!(sst.get(key(3)), Some(value(3)));
-            assert_eq!(sst.get(key(4)), Some(value(4)));
-            assert_eq!(sst.get(key(5)), Some(value(5)));
-        }
+    #[test]
+    fn test_sstabl_tombstones() {
+        let key = |i| format!("key-{}", i);
+        let value = |i| format!("value-{}", i);
+        let mut sst = SSTable::new("./test_tmp", 3);
+        assert!(sst.clear().is_ok());
+        (1..=5).for_each(|i| {
+            sst.insert(key(i), value(i)).expect("success");
+        });
+        sst.delete(key(2));
+        // restore WAL
+        // memtable: [4, 5], tombstone: [2], disktable: [1, 2, 3]
+        let mut sst = SSTable::new("./test_tmp", 3);
+        assert_eq!(sst.get(key(1)), Some(value(1)));
+        assert_eq!(sst.get(key(2)), None);
+        assert_eq!(sst.get(key(3)), Some(value(3)));
+        assert_eq!(sst.get(key(4)), Some(value(4)));
+        assert_eq!(sst.get(key(5)), Some(value(5)));
     }
 }
