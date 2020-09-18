03874c6325188b2a46fc64ac27fa0bfdac8b2297
petitviolet <violethero0820@gmail.com>
2020-09-18 23:29:46 +0900

    Fixed test

--- src/sst.rs
+++ src/sst.rs
@@ -93,7 +93,7 @@ mod tests {
     fn test_sstabl_tombstones() {
         let key = |i| format!("key-{}", i);
         let value = |i| format!("value-{}", i);
-        let mut sst = SSTable::new("./test_tmp", 3);
+        let mut sst = SSTable::new("./test_tmp2", 3);
         assert!(sst.clear().is_ok());
         (1..=5).for_each(|i| {
             sst.insert(key(i), value(i)).expect("success");
@@ -101,7 +101,7 @@ mod tests {
         sst.delete(key(2));
         // restore WAL
         // memtable: [4, 5], tombstone: [2], disktable: [1, 2, 3]
-        let mut sst = SSTable::new("./test_tmp", 3);
+        let sst = SSTable::new("./test_tmp2", 3);
         assert_eq!(sst.get(key(1)), Some(value(1)));
         assert_eq!(sst.get(key(2)), None);
         assert_eq!(sst.get(key(3)), Some(value(3)));
