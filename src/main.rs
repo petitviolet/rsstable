9d117bb72237688d1cc1b5a20ec05dc355d551fa
petitviolet <violethero0820@gmail.com>
2021-01-18 21:41:53 +0900

    Replaced log::info with println!

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ fn main() {
     let mut sst = SSTable::new("./tmp", 3);
     // sst.clear().expect("failed to clear");
     (1..10).for_each(|i| {
-        log::info!("i: {} =====", i);
+        println!("i: {} =====", i);
         let key = || format!("key-{}", i);
         let value = || format!("value-{}", i);
-        log::info!("get({}): {:?}", i, sst.get(key()));
-        log::info!("insert: {:?}", sst.insert(key(), value()));
-        log::info!("get({}): {:?}", i, sst.get(key()));
-        log::info!("get({}): {:?}", i + 4, sst.get(format!("key-{}", i + 4)));
+        println!("get({}): {:?}", i, sst.get(key()));
+        println!("insert: {:?}", sst.insert(key(), value()));
+        println!("get({}): {:?}", i, sst.get(key()));
+        println!("get({}): {:?}", i + 4, sst.get(format!("key-{}", i + 4)));
     });
     ()
 }
